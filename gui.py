e288c71a22e7eadfdbd995502a2d085b4fbeaa31
ultrabgmi2006-lab <ultrabgmi2006@gmail.com>
2026-03-08 17:17:29 +0530

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+root.title("Ultra Jarvis AI")
+
+root.geometry("400x250")
+
+label = tk.Label(
+    root,
+    text="JARVIS ONLINE",
+    font=("Arial",20)
+)
+
+label.pack(pady=90)
+
+root.mainloop()
